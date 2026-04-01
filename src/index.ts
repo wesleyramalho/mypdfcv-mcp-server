547af0b51947dc663e3eb211d0eef9908ffae929
Wesley Candido <wcandido@shopmonkey.io>
2026-04-01 09:31:23 -0300

    chore: bump to v1.0.0 with published @mypdfcv dependencies

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { generateResumeSchema, generateResume } from "./tools/generate-resume.js
 
 const server = new McpServer({
   name: "mypdfcv",
-  version: "0.1.0",
+  version: "1.0.0",
 });
 
 server.tool(
